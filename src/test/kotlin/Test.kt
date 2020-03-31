44c67eddb5333317ce46f9d5badd72736c331283
Danilo Pianini <danilo.pianini@gmail.com>
2020-04-01 00:23:22 +0200

    Remove VM detection

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -7,11 +7,6 @@ import javax.script.ScriptEngineManager
 
 class Test : StringSpec({
     "Reproduce Groovy bug 9493" {
-        if(System.getProperty("java.vm.name").contains("Eclipse OpenJ9 VM") {
-            println("The test is expected to fail with NPE under J9")
-        } else {
-            println("The test is expected to complete under HotSpot")
-        }
         val engine = ScriptEngineManager().getEngineByName("groovy")
         engine shouldNotBe  null
         engine.eval("5 * foo", mapOf("foo" to 4, "fiz" to 0).asBindings()) shouldBe 20
